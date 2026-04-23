3e9cdeb6836ea85098d7fb162b8e7fcc8e62bf77
ch3pyxx <molo4kovs@gmail.com>
2026-04-23 19:37:10 +0500

    logging info added

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher 
 import asyncio
+import logging
 from config import TOKEN
 
 from aiogram.filters import CommandStart
@@ -17,6 +18,7 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     try:
         asyncio.run(main())    
     except KeyboardInterrupt:
