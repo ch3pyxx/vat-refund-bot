490eddedaf7a1608d08d86e5bc0f75f8f835b3cc
ch3pyxx <molo4kovs@gmail.com>
2026-04-23 22:59:16 +0500

    some changes to main.py

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,18 +1,32 @@
-from aiogram import Bot, Dispatcher 
+from aiogram import Bot, Dispatcher, F 
 import asyncio
 import logging
 from config import TOKEN
 
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("oxae котахбас")
+    await message.answer("Доброго времени суток!")
+
+@dp.message(Command('help'))
+async def cmd_help(message: Message):
+    await message.answer("Это команда help")
+
+@dp.message(F.text == "Сколько будет 10 + 10?")
+async def calculatebot(message: Message):
+    value = get_twenty()
+    await message.answer(str(value))
+
+
+def get_twenty()-> int:
+    return 20
 
 async def main():
     await dp.start_polling(bot)
