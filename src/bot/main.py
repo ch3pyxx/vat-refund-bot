819da964c36cb363d564e0980dd6d304226c819a
ch3pyxx <molo4kovs@gmail.com>
2026-04-23 18:11:06 +0500

    testing bot and configuration

---
+++ src/bot/main.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher 
+import asyncio
+from config import TOKEN
+
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer("oxae котахбас")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())    
